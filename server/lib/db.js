ef6c0173a16aa05c47398de5ea6fde0e4e72b30c
vivek kumar verma <vivek23vermasrivastava@gmail.com>
2025-09-02 17:50:12 +0530

    connected backend with database

---
+++ server/lib/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+// Function to connect to the mongodb database
+
+export const connectDB = async () => {
+  try {
+    mongoose.connection.on("connected", () => {
+      console.log("Database Connected");
+    });
+
+    mongoose.connect(`${process.env.MONGODB_URI}/chat-app`);
+  } catch (error) {
+    console.log(error);
+    
+  }
+};
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import http from "http";
+import { connectDB } from "./lib/db.js";
 
 // Create Express app and HTTP server
 const app = express();
@@ -12,6 +13,9 @@ app.use(express.json({limit: "4mb"}));
 app.use(cors());
 
 app.use("/api/status", (req,res)=> res.send("Server is live"));
+
+// connect to MongoDB
+await connectDB();
 
 const PORT = process.env.PORT || 5000;
 
