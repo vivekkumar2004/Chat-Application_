0556d35c32732c32838f245fc21e163dff968557
vivek kumar verma <vivek23vermasrivastava@gmail.com>
2025-09-04 02:36:24 +0530

    Login function to handle user authentication and socket connection

--- client/context/AuthContext.jsx
+++ client/context/AuthContext.jsx
@@ -26,6 +26,31 @@ export const AuthProvider = ({ children }) => {
       toast.error(error.message )
     }
   }
+
+  // Login function to handle user authentication and socket connection
+
+  const login = async (state, credentials)=>{
+    try {
+      const { data } = await axios.post(`/api/auth/${state}`, credentials);
+      if(data.success){
+        setAuthUser(data.userData);
+        connectSocket(data.userData);
+        axios.defaults.headers.common["token"] = data.token;
+        setToken(data.token);
+        localStorage.setItem("token", data.token)
+        toast.success(data.message)
+      }
+      else{
+        toast.error(data.message)
+      }
+    } catch (error) {
+      toast.success(error.message)
+    }
+  }
+   
+
+
+
   // Connect socket function to handle socket connection and online users updates
   const connectSocket = (userData)=>{
     if(!userData || socket?.connected) return;
