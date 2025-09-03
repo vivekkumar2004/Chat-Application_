13108295dc11ea9ea3f3dd94fa33ee30cb53ffa0
vivek kumar verma <vivek23vermasrivastava@gmail.com>
2025-09-04 01:01:05 +0530

    implement functionality in client side

---
+++ client/context/AuthContext.jsx
@@ -0,0 +1,36 @@
+import { createContext, useState } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+
+const backendUrl = import.meta.env.VITE_BACKEND_URL;
+axios.defaults.baseURL = backendUrl;
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [token, setToken] = useState(localStorage.getItem("token"));
+  const [authUser, setAuthUser] = useState(null);
+  const [onlineUsers, setOnlineUsers] = useState([]);
+  const [socket, setSocket] = useState(null);
+
+  // Check if user is authenticated and if so, set the user data and connect the socket
+  const checkAuth = async () => {
+    try {
+      const { data } = await axios.get("/api/auth/check");
+      if (data.success) {
+        setAuthUser(data.user);
+      }
+    } catch (error) {
+        toast.error(error.message )
+    }
+  };
+
+  const value = {
+    axios,
+    authUser,
+    onlineUsers,
+    socket,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
