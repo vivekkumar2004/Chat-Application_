91bd2c7f83efcb60b80154809d3ac7ca7999d673
vivek kumar verma <vivek23vermasrivastava@gmail.com>
2025-09-04 02:42:18 +0530

    Logout function to handle user logout and socket disconnection

--- client/context/AuthContext.jsx
+++ client/context/AuthContext.jsx
@@ -47,9 +47,17 @@ export const AuthProvider = ({ children }) => {
       toast.success(error.message)
     }
   }
-   
 
-
+  // Logout function to handle user logout and socket disconnection
+  const logout = async ()=>{
+    localStorage.removeItem("token");
+    setToken(null);
+    setAuthUser(null);
+    setOnlineUsers([]);
+    axios.defaults.headers.common["token"] = null;
+    toast.success("Logged out successfully")
+    socket.disconnect();
+  }
 
   // Connect socket function to handle socket connection and online users updates
   const connectSocket = (userData)=>{
