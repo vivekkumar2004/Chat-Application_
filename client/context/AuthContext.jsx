516e16d19f19b87a41f12c1334d938884eb14708
vivek kumar verma <vivek23vermasrivastava@gmail.com>
2025-09-04 02:47:56 +0530

    update profile function to handle user profile updates

--- client/context/AuthContext.jsx
+++ client/context/AuthContext.jsx
@@ -59,6 +59,19 @@ export const AuthProvider = ({ children }) => {
     socket.disconnect();
   }
 
+  // update profile function to handle user profile updates
+  const updateProfile = async (body) =>{
+     try {
+      const { data } = await axios.put("/api/auth/update-profile", body);
+      if(data.success){
+        setAuthUser(data.user);
+        toast.success("Profile updated successfully ")
+      }
+     } catch (error) {
+      toast.error(error.message)
+     }
+  }
+
   // Connect socket function to handle socket connection and online users updates
   const connectSocket = (userData)=>{
     if(!userData || socket?.connected) return;
@@ -88,6 +101,9 @@ export const AuthProvider = ({ children }) => {
     authUser,
     onlineUsers,
     socket,
+    login,
+    logout,
+    updateProfile
   };
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
