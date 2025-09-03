7b10c608b13fbe3fed7844e81ae3e42028c85394
vivek kumar verma <vivek23vermasrivastava@gmail.com>
2025-09-03 11:13:35 +0530

    Implement authentication routes and integrate Cloudinary for image upload

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,7 @@
-import { generateToken } from "../lib/utils";
-import User from "../models/User";
+import { generateToken } from "../lib/utils.js";
+import User from "../models/User.js";
+import bcrypt from "bcryptjs"
+import cloudinary from "../lib/cloudinary.js"
 
 
 
@@ -60,4 +62,26 @@ export const login = async (req,res)=>{
 // Controller to check if user us authenticated
 export const checkAuth = (req,res)=>{
     res.json({success: true, user: req.user});
+}
+
+// Controller to update user profile details 
+export const updateProfile = async(req,res)=>{
+    try {
+        const{ profilePic, bio, fullName } = req.body;
+
+        const userId = req.user._id;
+        let updatedUser;
+        if(!profilePic){
+            updateProfile = await User.findByIdAndUpdate(userId, {bio,fullName}, {new:true});  
+        }
+        else{
+            const upload = await cloudinary.uploader.upload(profilePic);
+            updatedUser = await User.findByIdAndUpdate(userId, {profilePic: upload.secure_url, bio, fullName},{new: true});
+        }
+        res.json({success: true, user:updatedUser})
+    } catch (error) {
+        console.log(error.message);
+        
+        res.json({success: false, message: error.message})
+    }
 }
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import "dotenv/config";
 import cors from "cors";
 import http from "http";
 import { connectDB } from "./lib/db.js";
+import userRouter from "./routes/userRoutes.js";
 
 // Create Express app and HTTP server
 const app = express();
@@ -12,7 +13,9 @@ const server = http.createServer(app)
 app.use(express.json({limit: "4mb"}));
 app.use(cors());
 
+// Routes setup
 app.use("/api/status", (req,res)=> res.send("Server is live"));
+app.use("/api/auth", userRouter);
 
 // connect to MongoDB
 await connectDB();
