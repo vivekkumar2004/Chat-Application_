b931944796fc723219da66e2226bb1320482c1dd
vivek kumar verma <vivek23vermasrivastava@gmail.com>
2025-09-06 10:03:17 +0530

    server.js is updated for vercel

--- server/server.js
+++ server/server.js
@@ -9,47 +9,49 @@ import { Server } from "socket.io";
 
 // Create Express app and HTTP server
 const app = express();
-const server = http.createServer(app)
+const server = http.createServer(app);
 
 // Initialize socket.io server
 export const io = new Server(server, {
-    cors: {origin: "*"}
-})
+  cors: { origin: "*" },
+});
 
 // Store online users
-export const userSocketMap = {};  //{ userId: socketId }
+export const userSocketMap = {}; //{ userId: socketId }
 
 // Socket.io connection handler
-io.on("connection",(socket)=>{
-    const userId = socket.handshake.query.userId;
-    console.log("User Connected", userId);
+io.on("connection", (socket) => {
+  const userId = socket.handshake.query.userId;
+  console.log("User Connected", userId);
 
-    if(userId) userSocketMap[userId] = socket.id;
+  if (userId) userSocketMap[userId] = socket.id;
 
-    // Emit online users to all connected clients   
+  // Emit online users to all connected clients
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", userId);
+    delete userSocketMap[userId];
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
+});
 
-    socket.on("disconnect", ()=>{
-        console.log("User Disconnected", userId);
-        delete userSocketMap[userId];
-        io.emit("getOnlineUsers", Object.keys(userSocketMap))
-        
-    })
-    
-})
-
-// Middleware setup 
-app.use(express.json({limit: "4mb"}));
+// Middleware setup
+app.use(express.json({ limit: "4mb" }));
 app.use(cors());
 
 // Routes setup
-app.use("/api/status", (req,res)=> res.send("Server is live"));
+app.use("/api/status", (req, res) => res.send("Server is live"));
 app.use("/api/auth", userRouter);
 app.use("/api/messages", messageRouter);
 
 // connect to MongoDB
 await connectDB();
 
-const PORT = process.env.PORT || 5000;
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  server.listen(PORT, () => console.log("Server is running on PORT: " + PORT));
+}
 
-server.listen(PORT,()=> console.log("Server is running on PORT: " + PORT))
+// Export server for vercel
+export default server;
